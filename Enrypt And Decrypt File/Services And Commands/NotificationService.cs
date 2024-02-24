1c8be940979fda780d381a0b91502bc50dd0f42e
HuseynGPT <huseynovbabek1@gmail.com>
2024-02-24 19:53:32 +0400

    Add project files.

---
+++ Enrypt And Decrypt File/Services And Commands/NotificationService.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Enrypt_And_Decrypt_File;
+
+public abstract class NotificationService : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+
+    public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+                => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+
+
+}
---
+++ Enrypt And Decrypt File/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Enrypt_And_Decrypt_File.ViewModels;
+
+public class MainWindowViewModel:NotificationService
+{
+    private string? filePath;
+
+    public string? FilePath { get => filePath; set { filePath = value;OnPropertyChanged(); } }
+
+
+    public bool IsEncrypted { get; set; } = false;
+    public bool IsDecrypted { get; set; } = true;
+    public bool IsEncyrptRadioButtonChecked { get; set; }
+    public bool IsDecyrptRadioButtonChecked { get; set; }
+    public ICommand? SelectFileCommand { get; set; }
+    public ICommand? StartCommand { get; set; }
+
+
+
+
+    public MainWindowViewModel()
+    {
+        SelectFileCommand = new RelayCommand(SelectFile);
+        StartCommand = new RelayCommand(Start, CanStart);
+    }
+
+    private bool CanStart(object? obj)
+    {
+        if (FilePath == null)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private void Start(object? obj)
+    {
+        if (FilePath != string.Empty)
+        {
+
+            //Encrypt
+            if (IsEncyrptRadioButtonChecked == true && IsDecrypted == true)
+            {
+                string textFormFile = File.ReadAllText(FilePath!);
+                textFormFile = Convert.ToBase64String(Encoding.Unicode.GetBytes(textFormFile));
+                File.WriteAllText(FilePath!, textFormFile);
+                IsEncrypted = true;
+                
+
+            }
+
+            // decrypt
+            else if (IsDecyrptRadioButtonChecked == true && IsEncrypted == true)
+            {
+                string textFormFile = File.ReadAllText(FilePath!);
+                textFormFile = Encoding.Unicode.GetString(Convert.FromBase64String(textFormFile));
+                File.WriteAllText(FilePath!, textFormFile);
+                IsEncrypted = false;
+                IsDecrypted = true;
+            }
+        }
+        else
+            MessageBox.Show("You mmust select file");
+
+    }
+
+    private void SelectFile(object? obj)
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.Title = "Choose file";
+        openFileDialog.Filter = "All Files(*.*)|*.*";
+
+        if (openFileDialog.ShowDialog() == true)
+        {
+            FilePath = openFileDialog.FileName;
+        }
+        else
+        {
+            MessageBox.Show("File cannot choose");
+        }
+    }
+}
